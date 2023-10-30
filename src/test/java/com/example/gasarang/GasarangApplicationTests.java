780767459d456c12413bed800fd1e0c7c8cb049f
ChangSeongChu <jacob@puzzlesystems.co.kr>
2023-10-30 16:17:43 +0900

    first commit

---
+++ src/test/java/com/example/gasarang/GasarangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gasarang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasarangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
